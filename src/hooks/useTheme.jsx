c60561c54da20f72263349830ef7ee581b97b6d6
OLStark <78405223+starkov-sys@users.noreply.github.com>
2023-08-17 11:43:18 +0300

    Updates

--- src/hooks/useTheme.jsx
+++
@@ -1,9 +0,0 @@
-import { useContext } from "react";
-import { ThemeContext } from "../providers/ThemeProvider";
-
-const useTheme = () => {
-  const value = useContext(ThemeContext);
-  return value;
-};
-
-export default useTheme;
